bad9d816811495db226b7f31cec593121bb84eff
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2021-04-24 04:32:51 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+/*
+   clang main.c -std=c99 -O3 -Wall -lm -o main && ./main
+   clang main.c -std=c99 -O3 -Wall -Wextra -lm -o main && ./main
+   clang -std=c99 -E main.c
+*/
+#include "./include/santos_i_f_f.h"
+
+int main(int argc, char **argv){
+   return 0;
+}
