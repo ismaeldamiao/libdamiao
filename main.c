1ce82bd632782993a9bc882377d9c3468ee92631
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2021-04-24 04:55:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
    clang main.c -std=c99 -O3 -Wall -lm -o main && ./main
    clang main.c -std=c99 -O3 -Wall -Wextra -lm -o main && ./main
-   clang -std=c99 -E main.c
+   clang -std=c99 -E -P main.c
 */
 #include "./include/santos_i_f_f.h"
 
