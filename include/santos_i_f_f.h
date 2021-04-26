7335243d398e90609eb336598e4a1aca05cdc875
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2021-04-26 18:05:42 -0300

    Update santos_i_f_f.h

--- include/santos_i_f_f.h
+++ include/santos_i_f_f.h
@@ -1,6 +1,19 @@
+/* This file is a collection of functions and subroutine for use in
+   physics, mathematics and engineering.
+   This is conformed to the ISO/IEC 9899:1999 (also called C99), but
+   contains intructions to compile with some GNU extensions if __GNUC__ is
+   defined. */
 #ifndef SANTOS_I_F_F
 #define SANTOS_I_F_F 1
 
+// If possible, enable
+#ifdef __GNUC__
+# pragma GCC optimize ("O3") // O3 optimaization,
+# pragma GCC diagnostic warning "-Wall" // all warning and
+# pragma GCC diagnostic warning "-Wextra" // more warning.
+/* To test portabilit i enable informes if i use GCC extentions. */
+# pragma GCC diagnostic warning "-Wpedantic"
+#endif
 /* *****************************************************************************
    C standard libraries
 ***************************************************************************** */
@@ -8,11 +21,15 @@
 #include <stdlib.h>  /* Numeric conversion functions, memory allocation */
 #include <math.h>    /* Many mathematical functions */
 #include <complex.h> /* Many complex mathematical functions */
-//#include <tgmath.h> /* Do not use */
 #include <float.h>   /* DBL_MAX */
 #include <limits.h>  /* INT_MAX, INT_MIN */
 #include <time.h>    /* Defines date- and time-handling functions */
 #include <stdarg.h>  /* var-args */
+/* If you see the file tgmath.h you can note that the specifc term based on the
+   general term of a function is found using various tests. In a loop this is
+   very inneficient. Therefore do not use this. */
+#define _TGMATH_H 1
+//#include <tgmath.h>
 
 /* *****************************************************************************
    Numerical recipes in C
