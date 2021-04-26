f8cf6128c5b5c97ba65022fde224f25c3c020c05
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2021-04-26 18:09:07 -0300

    Delete sgn.c

--- include/SANTOS_I_F_F/math/sgn.c
+++
@@ -1,9 +0,0 @@
-/* ***
-   Compute the sign of a number.
-   https://en.wikipedia.org/wiki/Sign_function
-*** */
-double sgn(double x){
-   if(x > 0.0)      return  1.0;
-   else if(x < 0.0) return -1.0;
-   else             return  0.0;
-}
